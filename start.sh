2ae103e06236b91160c06c30e4c63102cf056acb
Devilmusicop <91986685+Devilmusicop@users.noreply.github.com>
2021-11-21 18:25:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/devilmusicop/devilxangle /devilxangle
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/devilmusicop/devilxangle -b $BRANCH /devilmusicop
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
